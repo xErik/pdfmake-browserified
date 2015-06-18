6bc7822f7eaf5dfb8757b7a30cd5c59286f288a1
xerik <thecoffeejunkie@hotmail.de>
2015-06-18 22:03:14 +0200

    NPM forgot server.js
    https://github.com/npm/npm/issues/5082

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname+'/test/')).listen(8080);
 console.log('http://localhost:8080');
-open('http://localhost:8080');
+open('http://localhost:8080'); 
